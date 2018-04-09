9f27a1324fbcb87af2839862878cd1bd33ef478a
Genevieve <genevieve.crow@submittable.com>
2018-04-09 17:04:32 -0600

    added youtube api and simple video search and display components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const SearchBar = () => {
-  return <input placeholder="What are you searching for?"/>
+class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      term: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={e => this.onInputChange(e.target.value)} />
+      </div>
+    )
+  }
+
+  onInputChange(term) {
+    this.setState({term})
+    this.props.onSearchTermChange(term)
+  }
 }
 
 export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import _ from 'lodash'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyDvc6k7qH0daF939_58z1JrMmi1K7WplHs';
 
-// Create a new component. This component produces HTML.
+ class App extends Component {
+   constructor(props) {
+     super(props)
 
- const App = () => {
-   return <SearchBar />
+     this.state = {
+       videos: [],
+       selectedVideo: null
+     }
+     this.videoSearch('kirk richardson')
+   }
+
+   videoSearch(term) {
+     YTSearch({key: API_KEY, term: term}, (videos) => {
+       this.setState({
+         videos: videos,
+         selectedVideo: videos[0]
+        })
+     })
+   }
+
+   render() {
+     // throttled serarch, function can only run every 300 milliseceonds
+     const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
+     return (
+       <div>
+         <SearchBar onSearchTermChange={videoSearch} />
+         <VideoDetail video={this.state.selectedVideo} />
+         <VideoList videos={this.state.videos}
+           onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
+       </div>
+     )
+   }
  }
 
 // Take this component's generated HTML and put it on the page.
